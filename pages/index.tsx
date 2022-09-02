2602de2148f000be53535422f97a8d0ec2af58eb
moogieon <vzlzydn5007@gmail.com>
2022-09-02 17:24:46 +0900

    소개 페이지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import ImageSlider from "../src/components/ImageSlider";
+import AboutMe from "../src/units/aboutMe";
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +15,8 @@ const Home: NextPage = () => {
 
       <main>
         <div w-text="blue-500">안녕 포토폴리오용</div>
-        <ImageSlider/>
+        <AboutMe />
+        <ImageSlider />
       </main>
 
       <footer>
---
+++ src/units/aboutMe.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const AboutMe: NextPage = () => {
+  return (
+    <div>
+      <p>about me</p>
+    </div>
+  );
+};
+
+export default AboutMe;
