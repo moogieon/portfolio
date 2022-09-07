d67c3bcf4518cf0818c61e5e10e8d433c5947cb3
moogieon <vzlzydn5007@gmail.com>
2022-09-07 16:51:24 +0900

    기술 페이지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,15 @@ import Head from "next/head";
 import Image from "next/image";
 import ImageSlider from "../src/components/ImageSlider";
 import AboutMe from "../src/units/aboutMe";
+import Used from "../src/units/used";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div w-text="blue-500">안녕 포토폴리오용</div>
+      <div w-text="blue-500">안녕 포토폴리오</div>
       <AboutMe />
       <ImageSlider />
+      <Used/>
     </>
   );
 };
---
+++ src/units/used.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Used: NextPage = () => {
+  return (
+    <div>
+      <p>사용 가능 기술</p>
+    </div>
+  );
+};
+
+export default Used;
