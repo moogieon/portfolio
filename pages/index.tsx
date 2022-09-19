af7f17aa02e794cda5aba6291f49f92c58ab1c2a
moogieon <vzlzydn5007@gmail.com>
2022-09-19 16:56:46 +0900

    css 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,21 @@ import Head from "next/head";
 import Image from "next/image";
 import ImageSlider from "../src/components/ImageSlider";
 import AboutMe from "../src/units/aboutMe";
+import Experiences from "../src/units/experiences";
 import Used from "../src/units/used";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div w-text="blue-500">안녕 포토폴리오</div>
-      <AboutMe />
-      <ImageSlider />
-      <Used/>
+      <div w-h="full" w-gradient="from-[#245e9d] to-t">
+        <div w-container="~" w-m="x-auto">
+          <h1 w-text="blue-500">안녕 포토폴리오</h1>
+          <AboutMe />
+          <Experiences />
+          <ImageSlider />
+          <Used />
+        </div>
+      </div>
     </>
   );
 };
