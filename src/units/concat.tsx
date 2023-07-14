4c741809995f0e2ab5ced15a1e466965a3c946c5
moogieon <vzlzydn5007@gmail.com>
2023-07-14 15:19:44 +0900

    멀티 폼 저장

--- src/units/concat.tsx
+++ src/units/concat.tsx
@@ -44,6 +44,93 @@ const Concat: NextPage = () => {
   //   fetchData();
   // }, []);
 
+  // JSON 데이터 가져오기
+  const [currentStep, setCurrentStep] = useState(1);
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const handleNextStep = () => {
+    if (validateForm()) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const handlePreviousStep = () => {
+    setCurrentStep(currentStep - 1);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log("Form data:", formData);
+    }
+  };
+  const handleCheckboxChange = (e, stepId, optionId) => {
+    const { checked } = e.target;
+    setFormData((prevData) => {
+      const updatedOptions = checked
+        ? [...(prevData[`step-${stepId}`] || []), optionId]
+        : (prevData[`step-${stepId}`] || []).filter((id) => id !== optionId);
+      return {
+        ...prevData,
+        [`step-${stepId}`]: updatedOptions,
+      };
+    });
+  };
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      setFormData((prevData) => {
+        const selectedOptions = prevData[name] || [];
+        const updatedOptions = checked
+          ? [...selectedOptions, value]
+          : selectedOptions.filter((option) => option !== value);
+
+        return {
+          ...prevData,
+          [name]: updatedOptions,
+        };
+      });
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: type === "radio" ? Number(value) : value,
+      }));
+    }
+  };
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    // 필드 유효성 검사 로직
+    // 필요한 경우 각 스텝에 맞게 유효성 검사를 추가하세요.
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://assets.cdn.soomgo.com/data/exam/mock/input_clean.json"
+      );
+
+      setFormData(response.data);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
+  useState(() => {
+    fetchData();
+  }, []);
+
+  if (!formData.items) {
+    return <div>Loading form...</div>;
+  }
+
+  const steps = formData.items;
   const submitFormAndShowCaptcha = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setToken(true);
@@ -66,6 +153,7 @@ const Concat: NextPage = () => {
         }
       );
   };
+  console.log(formData);
   return (
     <section w-h="min-100vh full" id="concat" w-pos="relative">
       <div
@@ -158,4 +246,69 @@ const Concat: NextPage = () => {
   );
 };
 
+{/* <div>
+<form onSubmit={handleFormSubmit}>
+  {steps.map((step, index) => {
+    if (index + 1 === currentStep) {
+      return (
+        <div key={step.itemId}>
+          <h3>{step.title}</h3>
+          {step.formType === "checkbox" && (
+            <div>
+              {step.options.map((option) => (
+                <label key={option.id}>
+                  <input
+                    type="checkbox"
+                    name={`step-${step.itemId}`}
+                    value={option.id}
+                    checked={(
+                      formData[`step-${step.itemId}`] || []
+                    ).includes(String(option.id))}
+                    onChange={(e) => handleInputChange(e)}
+                  />
+                  {option.text}
+                </label>
+              ))}
+            </div>
+          )}
+          {step.formType === "select" && (
+            <div>
+              {step.options.map((option) => (
+                <label key={option.id}>
+                  <input
+                    type="radio"
+                    name={`step-${step.itemId}`} // 동일한 name 속성 부여
+                    value={option.id}
+                    checked={
+                      formData[`step-${step.itemId}`] ===
+                      Number(option.id)
+                    }
+                    onChange={(e) => handleInputChange(e)}
+                  />
+                  {option.text}
+                </label>
+              ))}
+            </div>
+          )}
+     
+          {currentStep > 1 && (
+            <button type="button" onClick={handlePreviousStep}>
+              Previous
+            </button>
+          )}
+          {currentStep < steps.length && (
+            <button type="button" onClick={handleNextStep}>
+              Next
+            </button>
+          )}
+          {currentStep === steps.length && (
+            <button type="submit">Submit</button>
+          )}
+        </div>
+      );
+    }
+    return null;
+  })}
+</form>
+</div> */}
 export default Concat;
